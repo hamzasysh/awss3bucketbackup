cac5cc09d3aee333fb6c2748a6c8e22cdd9df418
M Hamza <hamzaashraf@systemheuristics.com>
2024-02-19 10:53:41 +0500

    fix : requested changes have been made

--- script.py
+++ script.py
@@ -1,72 +1,96 @@
 import subprocess
-import sys
+import os
 import boto3
+import logging
+from datetime import datetime
+from dotenv import load_dotenv
 
-def mongodump(database,collections,outpaths):
-     for i in range(len(collections)):
-          command = ["mongodump", "--uri", database, "--collection", collections[i],"--out",outpaths[i]]
-          try:
-              subprocess.run(command, check=True)
-              print("Backup completed successfully.")
-          except subprocess.CalledProcessError as e:
-              print(f"Backup failed: {e}")
+load_dotenv()
+logging.basicConfig(filename="app.log", level=logging.INFO)
 
-def uploadtos3(outpaths,bucket,obj_paths):
+
+def mongodump(database, outpath):
+    command = [
+        "mongodump",
+        "--uri",
+        database,
+        "--out",
+        outpath,
+    ]
     try:
-        s3 = boto3.client('s3')
-    except Exception as e:
-        print(f"s3 bucket client failed: {e}")
+        subprocess.run(command, check=True)
+        logging.info("Backup completed successfully.")
+    except subprocess.CalledProcessError as e:
+        logging.error(f"Backup failed: {e}")
 
-    for i in range(len(outpaths)):
-        try:
-            with open(outpaths[i], 'rb') as f:
-                s3.upload_fileobj(f, bucket, obj_paths[i])
-            print(f"File uploaded successfully to S3: s3://{bucket}/{obj_paths[i]}")
-        except Exception as e:
-            print(f"File upload to S3 failed: {e}")
 
-def download_from_s3(bucket, obj_paths, s3objpaths):
-    # Initialize the S3 client
+def uploadtos3(outpath, bucket):
+    s3 = boto3.client("s3")
+    current_date = datetime.now().date()
+    path = None
+    datestr = None
+
     try:
-        s3 = boto3.client('s3')
+        for i in range(1, 8):
+            temp = s3.list_objects_v2(Bucket=bucket, Prefix=f"{current_date}_{i}")
+            if "Contents" in temp:
+                datestr = f"{current_date}_{i}"
+        if datestr:
+            no = int(datestr.split("_")[1])
+            if no:
+                path = f"{current_date}_{no + 1}"
+            else:
+                path = f"{current_date}_1"
+        else:
+            path = f"{current_date}_1"
+
+        for root, _, files in os.walk(outpath):
+            for file in files:
+                local_path = os.path.join(root, file)
+                s3_path = f"{path}/{file}"
+                s3.upload_file(local_path, bucket, s3_path)
+                logging.info(f"File uploaded successfully: {s3_path}")
     except Exception as e:
-        print(f"s3 bucket client failed: {e}")
-
-    for i in range(len(obj_paths)):
-        try:
-            with open(s3objpaths[i], 'wb') as f:
-                s3.download_fileobj(bucket, obj_paths[i], f)
-            #s3.download_file(bucket, obj_paths[i],s3objpaths[i])
-            print(f"File downloaded successfully to: {s3objpaths[i]}")
-        except Exception as e:
-            print(f"File download from S3 failed: {e}")
+        logging.error(f"An error occurred: {e}")
 
 
-def mongorestore(uri,database2,collections,s3objpaths):
-    for i in range(len(collections)):
-        command = ["mongorestore", "--uri", uri, "--nsInclude", database2+"."+collections[i],s3objpaths[i]]
-        print(command)
-        try:
-            subprocess.run(command, check=True)
-            print("Backup completed successfully.")
-        except subprocess.CalledProcessError as e:
-            print(f"Backup failed: {e}")
+def download_from_s3(bucket, folder, s3objpath):
+    s3 = boto3.client("s3")
+    try:
+        objects = s3.list_objects_v2(Bucket=bucket, Prefix=folder)["Contents"]
+        for obj in objects:
+            key = obj["Key"]
+            with open(os.path.join(s3objpath, obj["Key"].split("/")[1]), "wb") as f:
+                s3.download_fileobj(bucket, key, f)
+            logging.info("file with key: " + key + " downloaded from s3")
+    except Exception as e:
+        logging.error(f"Backup failed: {e}")
+
+
+def mongorestore(uri, s3objpath):
+    command = [
+        "mongorestore",
+        "--uri",
+        uri,
+        s3objpath,
+    ]
+    try:
+        subprocess.run(command, check=True)
+        logging.info("Restore completed successfully.")
+    except subprocess.CalledProcessError as e:
+        logging.error(f"Restore failed: {e}")
 
 
 if __name__ == "__main__":
 
-    database = sys.argv[1]
-    collections = sys.argv[2].split(":")
-    outpaths=sys.argv[3].split(",")
-    bucket=sys.argv[4]
-    obj_paths=sys.argv[5].split(":")
-    s3objpaths=sys.argv[6].split(",")
-    uri=sys.argv[7]
-    database2=sys.argv[8]
+    source_uri = os.getenv("source_uri")
+    outpath = os.getenv("outpath")
+    bucket = os.getenv("bucket")
+    folder = os.getenv("folder")
+    s3objpath = os.getenv("s3objpath")
+    destination_uri = os.getenv("destination_uri")
 
-
-    mongodump(database, collections,outpaths)
-    uploadtos3(outpaths,bucket,obj_paths)
-    download_from_s3(bucket, obj_paths, s3objpaths)
-    mongorestore(uri,database2,collections,s3objpaths)
-
+    mongodump(source_uri, outpath)
+    uploadtos3(outpath, bucket)
+    download_from_s3(bucket, folder, s3objpath)
+    mongorestore(destination_uri, s3objpath)
